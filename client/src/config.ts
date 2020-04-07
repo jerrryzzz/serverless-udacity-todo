35ff1479a926b85ed6dd72850522de26c44d8db5
jerry <jerry.clarck@centraltest.com>
2020-04-07 16:15:00 +0200

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'b27bhm7b61'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+    domain: 'dev-q0swh64f.auth0.com', // Auth0 domain
+    clientId: 'i77lb6Smv0M6r1jCVHD6oCEEUflX5yJw', // Auth0 client id
+    callbackUrl: 'http://localhost:3000/callback',
+}
